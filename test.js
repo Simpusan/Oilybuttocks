e997131116b2b3cb363adcbcc9aca8e8fb7487ed
Oskar Edgren <simpa105@live.se>
2015-03-02 13:50:27 +0100

    Added initial files

---
+++ test.js
@@ -0,0 +1,10 @@
+document.getElementById("demo").innerHTML = "Can you see this text?";
+document.getElementById("demo").style.color = "blue";
+
+window.alert("vagina");
+
+if (window.jQuery) {  
+    document.getElementById("demo").innerHTML = "jQuery functional"; 
+} else {
+    document.getElementById("demo").innerHTML = "jQuery not functional";
+}
