b336118befe2b6c6e84e194251ce65fc713fb5f9
Oskar Edgren <simpa105@live.se>
2015-03-02 14:19:33 +0100

    Fixed parenthesis

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 document.getElementById("demo").innerHTML = "Can you see this text?";
 document.getElementById("demo").style.color = "blue";
 
-$(document).ready(function()) {
+$(document).ready(function() {
 	$('#vagina').fadeOut(5000);
 });
+
