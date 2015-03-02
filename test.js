ba3521b3017fdcd52f5d252dc1f407319f1ce7ac
Oskar Edgren <simpa105@live.se>
2015-03-02 14:15:05 +0100

    Indentation

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 document.getElementById("demo").innerHTML = "Can you see this text?";
 document.getElementById("demo").style.color = "blue";
 
-window.alert("vagina");
-
-if (window.jQuery) {  
-    document.getElementById("demo").innerHTML = "jQuery functional"; 
-} else {
-    document.getElementById("demo").innerHTML = "jQuery not functional";
-}
+$(document).ready(function()) {
+	$('#vagina').fadeOut(5000);
+});
