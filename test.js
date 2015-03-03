25271a0005fd2575530701df48e0c86deda9b759
Oskar Edgren <simpa105@live.se>
2015-03-03 09:58:06 +0100

    Removed unnecessary JS test

--- test.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("demo").innerHTML = "Can you see this text?";
-document.getElementById("demo").style.color = "blue";
-
-$(document).ready(function() {
-	$('#vagina').fadeOut(5000);
-});
-
